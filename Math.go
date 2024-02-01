3121bac474c61a11728bc4c319d0d38b34c765a5
mehsanmgh <ehsan_mq32@yahoo.com>
2024-02-01 19:36:16 +0330

    Project created

---
+++ Math.go
@@ -0,0 +1,9 @@
+package Math
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Sub(a, b float64) float64 {
+	return a - b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mehsanmgh/TestModule
+
+go 1.21.6
